c9ab61b2a59e561f760d815e3a4fb55947a73e91
Ryne Dalrymple <10953035@uvu.edu>
2025-08-31 21:13:14 -0600

    Initialize Spring Boot into project using https://start.spring.io into the backend directory.

---
+++ backend/src/test/java/com/cardcollector/cardhub/CardCollectorHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardcollector.cardhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardCollectorHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
